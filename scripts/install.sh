98a3b843607f524863ecee161757b6537b123101
Joerg Kritzen <j.kritzen@ontracon.de>
2019-01-09 14:50:19 +0100

    .

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+# Creating Directorys
+mkdir /opt/ontracon-demo-app
+mkdir /opt/ontracon-demo-app/static
+mkdir /opt/ontracon-demo-app/static/stylesheets
+mkdir /opt/ontracon-demo-app/templates
+# Install files
+cd /opt/ontracon-demo-app
+wget https://github.com/Ontracon/ontracon-demo-app/raw/master/ontracon-demo-app
+chmod 755 ontracon-demo-app
+cd /opt/ontracon-demo-app/static/stylesheets
+wget https://github.com/Ontracon/ontracon-demo-app/raw/master/static/stylesheets/background.jpeg
+wget https://github.com/Ontracon/ontracon-demo-app/raw/master/static/stylesheets/welcome-template.css
+cd /opt/ontracon-demo-app/templates
+wget https://github.com/Ontracon/ontracon-demo-app/raw/master/templates/welcome-template.html
+
+
 
